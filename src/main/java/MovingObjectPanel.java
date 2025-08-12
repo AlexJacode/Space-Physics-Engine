cec65090f7ba67a16764314b49c7fc179624b6b1
Alex <108822756+AlexJacode@users.noreply.github.com>
2025-08-12 15:05:59 -0400

    Update MovingObjectPanel.java

--- src/main/java/MovingObjectPanel.java
+++ src/main/java/MovingObjectPanel.java
@@ -7,14 +7,14 @@ public class MovingObjectPanel extends JPanel {
 
     public MovingObjectPanel(MovingObject object) {
         this.object = object;
-        setPreferredSize(new java.awt.Dimension(600, 400));  // Set preferred size
+        setPreferredSize(new java.awt.Dimension(600, 400));  // Set Size 
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillOval((int)object.getX(), (int)object.getY(), 20, 20);  // Draw the object as a red circle
+        g.fillOval((int)object.getX(), (int)object.getY(), 20, 20);  // Draw Object 
     }
 
     public void update() {
